81309d8f58acdd62df00b9801d30f86484f585bc
Farooquekk <22sw040@students.muet.edu.pk>
2025-07-06 03:45:52 -0700

    Tom and Jerry character quiz script
    Introduces a Python script that asks the user three questions to determine if they are like Tom, the cat from Tom and Jerry. The script checks the user's answers and prints a result based on their responses.

---
+++ 04_Tom_and_Jerry.py
@@ -0,0 +1,10 @@
+# Are you Tom ? Friend of jerry
+
+ans1 = input('You always fight with Jerry 🐭 ? : '  )
+ans2 = input('You don\'t like Spike 🐶 ? : '  )
+ans3 = input('You always wants to sleep 💤 and eat 🍴 ? : '  )
+
+if(ans1.lower() == 'yes' and ans2.lower() == 'yes' and ans3.lower() == 'yes'):
+    print('Yes You are the 🐱‍👓 friend of 🐭')
+else:
+    print('No you are not a friend. ')
