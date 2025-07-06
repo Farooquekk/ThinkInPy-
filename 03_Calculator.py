838c43824059fe328c33f9dd7cd1dc8d05fea58e
Farooquekk <22sw040@students.muet.edu.pk>
2025-07-06 03:45:25 -0700

    Python calculator
    Introduces a simple calculator that takes two integer inputs and an operator (+, -, *, /) from the user, then performs the corresponding arithmetic operation and displays the result.

---
+++ 03_Calculator.py
@@ -0,0 +1,23 @@
+print('***  WELCOME TO THE PYTHON CALCULATOR  ***')
+
+operator_1 = int(input('Enter Operator 1 : '))
+operator_2 = int(input('Enter Operator 2 : '))
+operand = input('       Enter Operand (+,-,*,/) : ')
+
+
+if operand == '+':
+    print(f'Sum of {operator_1} and {operator_2} : {operator_1+operator_2}')
+
+elif operand == '-':
+    print(f'Difference of {operator_1} and {operator_2} : {operator_1-operator_2}')
+
+elif operand == '*':
+    print(f'Difference of {operator_1} and {operator_2} : {operator_1*operator_2}')
+
+elif operand == '/':
+    print(f'Division of {operator_1} and {operator_2} : {operator_1/operator_2}')
+
+else:
+    print('Invalid Operation!!')
+
+
